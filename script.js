cbd29ca64abf6b5425d1af070867467f04c2ec74
Althea <altheagene@users.noreply.github.com>
2026-02-12 13:48:33 +0800

    Updated script.js. Added the section elements for DOM Manipulation.

---
+++ script.js
@@ -0,0 +1,12 @@
+let currentUser = null;
+
+//COMPONENTS
+const homePage = document.getElementById('home-page');
+const registerPage = docoument.getElementById('register-page')
+const verifyEmailPage = document.getElementById('verify-email-page');
+const loginPage = document.getElementById('login-page');
+const profilePage = document.getElementById('profile-page');
+const employeesPage = document.getElementById('employees-page');
+const departmentsPage = document.getElementById('departments-page')
+const accountsPage = document.getElementById('accounts-page');
+const requestsPage = document.getElementById('requests-page')
