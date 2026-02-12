fac240fc5cbea0dee02be5028fd4af4a9f55bf5f
Althea <altheagene@users.noreply.github.com>
2026-02-12 23:46:54 +0800

    Updated script.js Added hashchange event listener and navigateTo() function for hash changes.

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
-let currentUser = null;
+window.location.hash = '#/'
 
 //COMPONENTS
 const homePage = document.getElementById('home-page');
-const registerPage = docoument.getElementById('register-page')
+const registerPage = document.getElementById('register-page')
 const verifyEmailPage = document.getElementById('verify-email-page');
 const loginPage = document.getElementById('login-page');
 const profilePage = document.getElementById('profile-page');
 const employeesPage = document.getElementById('employees-page');
 const departmentsPage = document.getElementById('departments-page')
 const accountsPage = document.getElementById('accounts-page');
-const requestsPage = document.getElementById('requests-page')
+const requestsPage = document.getElementById('requests-page');
+console.log('hi')
+
+let currentUser = null;
+let currentPage = homePage;
+
+function navigateTo(hash){
+    currentPage.classList.remove('active')
+
+    switch(hash){
+        case '#/register-page' : currentPage = registerPage; break;
+        case '#/login-page':   currentPage = loginPage; break;
+        case '#/verify-email-page' : currentPage = verifyEmailPage; break;
+        case '#/profile-page' : currentPage = profilePage; break;
+    }   
+    console.log(currentPage)
+    currentPage.classList.add('active');
+}
+
+window.addEventListener("hashchange", () => {navigateTo(window.location.hash);});
